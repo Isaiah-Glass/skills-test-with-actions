c676421c4e48e26684dfd733dc83a773e04b3e9b
Isaiah Glass <glass.364@osu.edu>
2026-04-21 16:24:42 +0000

    Reenable unit test that was disabled

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -34,17 +34,6 @@ def test_area_of_circle_zero_radius():
     assert result == 0
 
 
-def test_get_nth_fibonacci_ten():
-    """Test with n=10."""
-    # Arrange
-    n = 10
-
-    # Act
-    result = get_nth_fibonacci(n)
-
-    # Assert
-    assert result == 89
-
 def test_get_nth_fibonacci_one():
     """Test with n=1."""
     # Arrange
@@ -55,3 +44,15 @@ def test_get_nth_fibonacci_one():
 
     # Assert
     assert result == 1
+
+
+ def test_get_nth_fibonacci_ten():
+     """Test with n=10."""
+      #Arrange
+     n = 10
+
+      #Act
+     result = get_nth_fibonacci(n)
+
+      #Assert
+     assert result == 89
